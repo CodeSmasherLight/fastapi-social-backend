0120cc894ce18f5fa3dab002029082f69a4561e6
Satyam Singh <singhsatyam8383@gmail.com>
2025-11-01 22:06:17 +0530

    some basic changes

--- app/models.py
+++ app/models.py
@@ -7,8 +7,8 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String(50), nullable=False)
-    content = Column(String(100), nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
     published = Column(Boolean, server_default="True", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
@@ -19,8 +19,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(100), nullable=False, unique=True)
-    password = Column(String(100), nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
 
 class Vote(Base):
