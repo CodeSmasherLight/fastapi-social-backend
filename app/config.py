96d0f5a350207a6b855fdad8f2b921fd69d2e306
Satyam Singh <singhsatyam8383@gmail.com>
2025-11-01 19:21:04 +0530

    for deployment purpose

--- app/config.py
+++ app/config.py
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
     database_name: str
     database_username: str
     secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
     class Config:
         env_file = ".env"
