1ecdc2d1e632c875a7fd65a2b8052c48bb0dc5b4
Satyam Singh <singhsatyam8383@gmail.com>
2025-11-10 04:41:29 +0530

    added all necessary user related tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,21 +1,62 @@
-from fastapi.testclient import TestClient
-from app.main import app
+import pytest
 from app import schemas
+from app.config import settings
+import jwt
 
 
-client = TestClient(app)
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "satyam@gmail.com",
+                 "password": "qwerty"}
+    res = client.post("/users/", json=user_data)
 
-def test_read_root():
-    res = client.get("/")
-    # print(res.json().get("Hello"))
-    assert res.status_code == 200
-    assert res.json() == {"Hello": "World"}
+    assert res.status_code == 201
+    print(res.json())
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+
+    return new_user
 
 
-def test_create_user():
+# def test_read_root(client):
+#     res = client.get("/")
+#     # print(res.json().get("Hello"))
+#     assert res.status_code == 200
+#     assert res.json() == {"Hello": "World"}
+
+
+def test_create_user(client):
     res = client.post(
         "/users/", json={"email": "satyam@gmail.com", "password": "qwerty"})
     
     new_user = schemas.UserOutput(**res.json())
     assert new_user.email == "satyam@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'qwerty', 403),
+    ('satyam@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'qwerty', 403), # on testing with None value, it is expecting 403 instead of 422 hence changed the expected status code
+    ('satyam@gmail.com', None, 403) # same here
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    assert res.json().get('detail') == 'Invalid Credentials'    
