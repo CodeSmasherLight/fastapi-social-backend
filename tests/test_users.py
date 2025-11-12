a80d2b5bef44e90b04c41b467b6cc759481060e2
Satyam Singh <singhsatyam8383@gmail.com>
2025-11-12 22:06:55 +0530

    adding first github action

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,19 +4,19 @@ from app.config import settings
 import jwt
 
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "satyam@gmail.com",
-                 "password": "qwerty"}
-    res = client.post("/users/", json=user_data)
+# @pytest.fixture
+# def test_user(client):
+#     user_data = {"email": "satyam@gmail.com",
+#                  "password": "qwerty"}
+#     res = client.post("/users/", json=user_data)
 
-    assert res.status_code == 201
-    print(res.json())
+#     assert res.status_code == 201
+#     print(res.json())
 
-    new_user = res.json()
-    new_user['password'] = user_data['password']
+#     new_user = res.json()
+#     new_user['password'] = user_data['password']
 
-    return new_user
+#     return new_user
 
 
 # def test_read_root(client):
